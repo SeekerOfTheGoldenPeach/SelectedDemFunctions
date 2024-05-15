e67ac2dc17351353465cba06f3983f299d8365fd
Oktaw <oktawian512@gmail.com>
2024-05-15 13:03:53 +0200

    Adds default statements to Dem switches
    Removes multiple return statements from functions
    Switches default statements leds to return E_NOK
    Adds helper functions for reading, writing and clearing
    memory blocks by NvM

--- PoC/Core/Src/Dem.c
+++ PoC/Core/Src/Dem.c
@@ -1,96 +1,147 @@
 #include "Dem.h"
 #undef DLT_LOG_CONTEX
 #define DLT_LOG_CONTEX "DEM"
-Std_ReturnType Dem_SetEventStatus(Dem_EventIdType EventId, Dem_EventStatusType EventStatus)
-{
-    LOGF(DL_DEBUG, "Operation started!");
-    struct event event;
-    const void *pointerToData;
-    uint8_t data[4] = {0};
-    if (EventStatus == DEM_EVENT_STATUS_FAILED)
-    {
-        switch (EventId)
-        {
-        case OVER_VOLTAGE_ID:
-            event = overVoltage;
-            break;
 
-        case UNDER_VOLTAGE_ID:
-            event = underVoltage;
-            break;
-        }
-        LOGF(DL_DEBUG, "Got %s notification!", event.EventName);
-        event.EventStatus = EventStatus;
-        data[0] = event.EventId;
-        data[1] = event.EventStatus;
-        data[2] = 0xff;
-        data[3] = 0xff;
-        pointerToData = &data;
-        LOGF(DL_DEBUG, "Operation succeeded! Data passed to write: %x %x %x %x", data[0], data[1], data[2], data[3]);
-        LOGF(DL_DEBUG, "Start writing to NvM");
-        if (NvM_WriteBlock(event.blockId, pointerToData) == E_OK)
-        {
-            return E_OK;
-        }
+Std_ReturnType prepareEventDataForNvm(event event, Dem_EventStatusType EventStatus)
+{
+    Std_ReturnType ret_val = E_NOK;
+    uint8_t data[4] = {0};
+    const void *pointerToData = &data;
+
+    LOGF(DL_DEBUG, "Got %s notification!", event.EventName);
+    event.EventStatus = EventStatus;
+    data[0] = event.EventId;
+    data[1] = event.EventStatus;
+    data[2] = 0xff;
+    data[3] = 0xff;
+    LOGF(DL_DEBUG, "Operation succeeded! Data passed to write: %x %x %x %x", data[0], data[1], data[2], data[3]);
+    LOGF(DL_DEBUG, "Start writing to NvM");
+    if (NvM_WriteBlock(event.blockId, pointerToData) == E_OK)
+    {
+        ret_val = E_OK;
     }
-    LOGFF(DL_FATAL, "Operation failed!");
-    return E_NOK;
+    else
+    {
+        LOGFF(DL_FATAL, "Operation failed!");
+        ret_val = E_NOK;
+    }
+    return ret_val;
 }
 
-Std_ReturnType Dem_GetStatusOfDTC(uint8_t ClientId, uint8_t *DTCStatus)
+Std_ReturnType readEventDataFromNvm(event event, uint8_t *DTCStatus)
 {
-    LOGF(DL_DEBUG, "Operation started!");
-    struct event event;
-
-    switch (ClientId)
-    {
-    case OVER_VOLTAGE_ID:
-        event = overVoltage;
-        break;
-
-    case UNDER_VOLTAGE_ID:
-        event = underVoltage;
-        break;
-    }
-
+    Std_ReturnType ret_val = E_NOK;
     LOGF(DL_DEBUG, "Requesting status: %s", event.EventName);
     if (NvM_ReadBlock(event.blockId, DTCStatus) == E_OK)
     {
         LOGL(DL_INFO, "DEM: %s status: %x %x %x %x", event.EventName, DTCStatus[0], DTCStatus[1], DTCStatus[2], DTCStatus[3]);
         LOGF(DL_DEBUG, "Operation finished!");
 
-        return E_OK;
+        ret_val = E_OK;
     }
-
-    LOGFF(DL_FATAL, "Operation failed!");
-    return E_NOK;
+    else
+    {
+        LOGFF(DL_FATAL, "Operation failed!");
+        ret_val = E_NOK;
+    }
+    return ret_val;
 }
 
-Std_ReturnType Dem_ClearDTC(uint8_t ClientId)
+Std_ReturnType clearEventBlock(event event)
+{
+    uint8_t emptyData[4] = {0};
+    Std_ReturnType ret_val = E_NOK;
+
+    LOGF(DL_DEBUG, "Clear: %s  | BlockId: ", event.EventName, event.blockId);
+    if (NvM_WriteBlock(event.blockId, emptyData) == E_OK)
+    {
+        LOGF(DL_DEBUG, "Operation finished!");
+        ret_val = E_OK;
+    }
+    else
+    {
+        LOGFF(DL_FATAL, "Operation failed!");
+        ret_val = E_NOK;
+    }
+    return ret_val;
+}
+
+Std_ReturnType Dem_SetEventStatus(Dem_EventIdType EventId, Dem_EventStatusType EventStatus)
 {
     LOGF(DL_DEBUG, "Operation started!");
-    uint8_t emptyData[4] = {0};
-    struct event event;
+    struct event event = {"EmptyEvent", 0xFF, 0xFF, 0xFF, 0xFF};
+
+    Std_ReturnType ret_val = E_NOK;
+
+    if (DEM_EVENT_STATUS_FAILED == EventStatus)
+    {
+        switch (EventId)
+        {
+        case OVER_VOLTAGE_ID:
+            event = overVoltage;
+            ret_val = sendDataToNvM(event, EventStatus);
+            break;
+
+        case UNDER_VOLTAGE_ID:
+            event = underVoltage;
+            ret_val = sendDataToNvM(event, EventStatus);
+            break;
+
+        default:
+            break;
+        }
+    }
+    return ret_val;
+}
+
+Std_ReturnType Dem_GetStatusOfDTC(uint8_t ClientId, uint8_t *DTCStatus)
+{
+    LOGF(DL_DEBUG, "Operation started!");
+    struct event event = {"EmptyEvent", 0xFF, 0xFF, 0xFF, 0xFF};
+    Std_ReturnType ret_val = E_NOK;
 
     switch (ClientId)
     {
     case OVER_VOLTAGE_ID:
         event = overVoltage;
+        ret_val = readEventDataFromNvm(event, DTCStatus);
         break;
 
     case UNDER_VOLTAGE_ID:
         event = underVoltage;
+        ret_val = readEventDataFromNvm(event, DTCStatus);
+        break;
+
+    default:
         break;
     }
-    LOGF(DL_DEBUG, "Clear: %s  | BlockId: ", event.EventName, event.blockId);
-    if (NvM_WriteBlock(event.blockId, emptyData) == E_OK)
+    return ret_val;
+}
+
+Std_ReturnType Dem_ClearDTC(uint8_t ClientId)
+{
+    LOGF(DL_DEBUG, "Operation started!");
+    Std_ReturnType ret_val = E_NOK;
+
+    struct event event = {"EmptyEvent", 0xFF, 0xFF, 0xFF, 0xFF};
+
+    switch (ClientId)
     {
-        LOGF(DL_DEBUG, "Operation finished!");
-        return E_OK;
+    case OVER_VOLTAGE_ID:
+        event = overVoltage;
+        ret_val = clearEventBlock(event);
+        break;
+
+    case UNDER_VOLTAGE_ID:
+        event = underVoltage;
+        ret_val = clearEventBlock(event);
+        break;
+
+    default:
+        break;
     }
 
-    LOGFF(DL_FATAL, "Operation failed!");
-    return E_NOK;
+    return ret_val;
 }
 
 #undef DLT_LOG_CONTEX
