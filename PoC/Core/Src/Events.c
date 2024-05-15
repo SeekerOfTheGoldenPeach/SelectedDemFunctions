eec94917acdb51a933791b0dbdd63cdc704e5539
Oktaw <oktawian512@gmail.com>
2024-05-15 12:11:52 +0200

    format long lines

--- PoC/Core/Src/Events.c
+++ PoC/Core/Src/Events.c
@@ -1,4 +1,6 @@
 #include "Events.h"
 
-struct event overVoltage = {"OverVoltage", OVER_VOLTAGE_ID, DEM_EVENT_STATUS_PASSED, OVER_VOLTAGE_BOCK_ID, OVER_VOLTAGE_ID};
-struct event underVoltage = {"UnderVoltage", UNDER_VOLTAGE_ID, DEM_EVENT_STATUS_PASSED, UNDER_VOLTAGE_BOCK_ID, UNDER_VOLTAGE_ID};
+struct event overVoltage = {"OverVoltage", OVER_VOLTAGE_ID, DEM_EVENT_STATUS_PASSED,
+                            OVER_VOLTAGE_BOCK_ID, OVER_VOLTAGE_ID};
+struct event underVoltage = {"UnderVoltage", UNDER_VOLTAGE_ID, DEM_EVENT_STATUS_PASSED,
+                             UNDER_VOLTAGE_BOCK_ID, UNDER_VOLTAGE_ID};
